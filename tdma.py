9fd3cd48630815ece8889d2c4f142d72acd60a41
Charles Phillip Cloud <cpcloud@gmail.com>
2012-03-13 16:53:59 -0400

    added some fancy sphinx math doc

--- tdma.py
+++ tdma.py
@@ -2,11 +2,12 @@ import numpy as np
 import _tdma
 
 def tdma(A, b):
-    """Tridiagonal matrix solve.
+    """Tridiagonal matrix solver using a Python C extension to make it very
+    fast.
     
     Arguments:
     - `A`: Tridiagonal system of equations.
-    - `b`: Right hand side of A * x = b.
+    - `b`: Right hand side of :math:`\mathbf{Ax}=\mathbf{b}`.
     """
     lower = np.hstack((0, np.diag(A, -1)))
     middle = np.diag(A)
