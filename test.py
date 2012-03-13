6f1d33b52d19539f47b9d172029b44dfe486bfff
Charles Phillip Cloud <cpcloud@gmail.com>
2012-03-13 02:28:55 -0400

    fiddling with test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 from time import time
 from scipy.sparse import spdiags
-from pylab import *
+import numpy as np
 
 # C code import: see tdma.c
 from tdma import tdma
@@ -12,27 +12,29 @@ def test_tdma(n):
     Arguments:
     - `n`: Size of the matrix to test
     """
-    A = np.abs(randn(n, n))
+    A = np.abs(np.random.randn(n, n))
     
-    l = diag(A, -1)
-    d = diag(A)
-    u = diag(A, 1)
+    l = np.diag(A, -1)
+    d = np.diag(A)
+    u = np.diag(A, 1)
 
-    l1 = hstack((l, 0))
-    l2 = hstack((0, l))
+    l1 = np.hstack((l, 0))
+    l2 = np.hstack((0, l))
 
-    u1 = hstack((0, u))
-    u2 = hstack((u, 0))
+    u1 = np.hstack((0, u))
+    u2 = np.hstack((u, 0))
     
-    x = randn(n)
-    A = spdiags(vstack((l1, d, u1)),
+    x = np.random.randn(n)
+    A = spdiags(np.vstack((l1, d, u1)),
                        [-1, 0, 1], n, n).todense()
-    rhs = dot(A, x)
+    rhs = np.dot(A, x)
     tic = time()
     xhat = tdma(l2, d, u2, rhs)
     toc = time() - tic
-    return norm(x - xhat), toc * 1000, norm(x - xhat) < finfo(x.dtype).eps
+    return (np.linalg.norm(x - xhat),
+            toc * 1000,
+            np.linalg.norm(x - xhat) < np.finfo(x.dtype).eps)
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 10
     print 'error: {0:.4g}, runtime: {1:.4g} ms, {nxn:,} elements, less than eps: {2}'.format(*test_tdma(n), nxn=n ** 2)
