0ea3ddc30a535ad8f8aeb3e6260d4001ec0877fe
Charles Phillip Cloud <cpcloud@gmail.com>
2012-03-13 03:03:39 -0400

    fixed np namespace issue

--- tdma.py
+++ tdma.py
@@ -9,7 +9,7 @@ def tdma(A, b):
     - `b`: Right hand side of A * x = b.
     """
     lower = np.hstack((0, np.diag(A, -1)))
-    middle = diag(A)
-    upper = np.hstack((np.diag(A, 1)), 0)
+    middle = np.diag(A)
+    upper = np.hstack((np.diag(A, 1), 0))
     return _tdma.tdma(lower, middle, upper, b)
 
