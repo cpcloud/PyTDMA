9fa17cb95f9a4ed1f091955c0dc603a8ac4ae27f
Phillip Cloud <cpcloud@gmail.com>
2012-05-25 12:33:47 -0400

    got rid of single function imports in favor of whole module

--- test.py
+++ test.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 
-# Builtins
+import argparse
 from time import time
 
-# numpy/scipy
-from numpy import abs, diag, hstack, vstack, dot, finfo
-from numpy.random import randn
-from numpy.linalg import norm
-from scipy.sparse import spdiags
+import numpy as np
+import scipy
+
+from sklearn.utils.extmath import safe_sparse_dot, norm
 
 # solver
 from tdma import tdma
 
 def test_tdma(n):
     """Test the tridiagonal matrix solver.
-    
-    Arguments:
-    - `n`: Size of the matrix to test
+
+    Parameters
+    ----------
+    n : int
+        Size of the matrix to test
     """
-    A = randn(n, n)
-    l = hstack((diag(A, -1), 0))
-    d = diag(A)
-    u = hstack((0, diag(A, 1)))
-    
-    x = randn(n)
-    A = spdiags(vstack((l, d, u)),
-                [-1, 0, 1], n, n).todense()
-    
-    rhs = dot(A, x)
-    
+    l = np.hstack((np.random.randn(n - 1), 0))
+    d = np.random.randn(n)
+    u = np.hstack((0, np.random.randn(n - 1)))
+
+    x = np.random.randn(n)
+    A = scipy.sparse.spdiags(np.vstack((l, d, u)),
+                             [-1, 0, 1], n, n)
+
+    b = safe_sparse_dot(A, x)
+
     tic = time()
-    xhat = tdma(A, rhs)
+    xhat = tdma(A, b)
     toc = time() - tic
-    print 'error: {0:.4g}, runtime: {1:.4g} ms, {nxn:,} elements'.format(norm(x - xhat), toc * 1000, nxn=n ** 2)
-    
+
+    print 'error: {0:.4g}, runtime: {1:.4g} ms, {2:,} elements'.format(norm(x - xhat),
+                                                                       toc * 1000,
+                                                                       n ** 2)
+
 if __name__ == '__main__':
-    import argparse
+
     parser = argparse.ArgumentParser(description='PyTDMA testing script.')
     parser.add_argument('n',
                         metavar='N',
@@ -44,5 +47,3 @@ if __name__ == '__main__':
                         help='an integer indicating the number of rows and columns of the testing matrix')
     args = parser.parse_args()
     test_tdma(args.n[0])
-    
-    
