ed7b74cda39402577c0f0579e2d97961f2fc1a83
Phillip Cloud <cpcloud@gmail.com>
2014-04-21 12:40:43 -0400

    BLD: add np.get_include()

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 from Cython.Distutils import build_ext
 from Cython.Distutils.extension import Extension
+import numpy as np
 
-extra_compile_args = ['-march=native', '-O3']
+extra_compile_args = []
 extra_link_args = []
-ext_modules = [Extension('_tdma',
-                         ['tdma.pyx'],
+ext_modules = [Extension('_tdma', ['tdma.pyx'],
                          extra_compile_args=extra_compile_args,
                          extra_link_args=extra_link_args)]
 
 setup(name='tdma',
       cmdclass={'build_ext': build_ext},
+      include_dirs=[np.get_include()],
       ext_modules=ext_modules)
